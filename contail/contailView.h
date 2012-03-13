1d47f5c7a359a2debfad98a2f797bb8260f9a07b
Edward Marczak <marczak@radiotope.com>
2012-03-13 09:20:16 -0400

    Initial Commit

---
+++ contail/contailView.h
@@ -0,0 +1,13 @@
+//
+//  contailView.h
+//  contail
+//
+//  Created by Edward Marczak on 3/13/12.
+//  Copyright (c) 2012 Radiotope. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface contailView : ScreenSaverView
+
+@end
---
+++ contail/contailView.m
@@ -0,0 +1,52 @@
+//
+//  contailView.m
+//  contail
+//
+//  Created by Edward Marczak on 3/13/12.
+//  Copyright (c) 2012 Radiotope. All rights reserved.
+//
+
+#import "contailView.h"
+
+@implementation contailView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
